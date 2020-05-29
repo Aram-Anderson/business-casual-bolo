24a0f907062e9bb439913067e892917d85cda51b
aram anderson <aram987@gmail.com>
2020-05-29 12:11:19 -0600

    Updates

---
+++ precache-manifest.74012e613f38c2daf55281d8e98fc551.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c82c34d05a62405ef40437ce749100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24407639c4cd747ce76e",
+    "url": "/static/css/main.156358a8.chunk.css"
+  },
+  {
+    "revision": "be915017bb8feea944c6",
+    "url": "/static/js/2.0d973eb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d973eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24407639c4cd747ce76e",
+    "url": "/static/js/main.adb1fc6d.chunk.js"
+  },
+  {
+    "revision": "dd96caaaba57b788b659",
+    "url": "/static/js/runtime-main.30d7f8a8.js"
+  },
+  {
+    "revision": "f43910845eafc8cd8c07137f87ade8a7",
+    "url": "/static/media/oranges.f4391084.jpeg"
+  }
+]);
